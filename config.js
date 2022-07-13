ec9b4241473309a1369a499fbd0b0873dc371a56
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-07-13 22:03:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    token: "OTk2ODExMDkyMTIwNTY3ODA4.G8_zW9.E3LJLWDFoeLEoIhbbARpHX3w-sH_xj6Uqadsso",
     channel: "939367351063814184",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}\n\n***UJJWAL OP***",
     channel_id: "UCX8pnu3DYUnx8qy8V_c6oHg",
