3e809de4a203b37b19f881686079039ff5f53dbf
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-07-13 21:52:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
+    token: "OTk2ODExMDkyMTIwNTY3ODA4.G8_zW9.E3LJLWDFoeLEoIhbbARpHX3w-sH_xj6Uqadsso",
+    channel: "939367351063814184",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: "UCX8pnu3DYUnx8qy8V_c6oHg",
     watchInterval: 30000
 }
